01fe96ec764413dfa6615a26347b72afb37a1b76
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-02-10 19:27:48 -0500

    Tested the deeponet accuracy more than 10 stdev's away from the mean, perfect results!

--- FD_Deeponet/TestDeepONet.py
+++ FD_Deeponet/TestDeepONet.py
@@ -3,15 +3,15 @@ import json
 import gc
 
 import numpy as np
+import numpy.linalg as lg
 import scipy as sc
+import numpy.random as rd
 import matplotlib.pyplot as plt
 
 from DeepONet import DeepONet
 from Dataset import DeepONetDataset
 import ElasticPDE as pde
 import GaussianProces as gp
-
-import argparse
     
 # No need for gradients in test script
 pt.set_grad_enabled(False)
@@ -23,6 +23,7 @@ config_file = 'DataConfig.json'
 config = json.load(open(config_file))
 store_directory = config['Store Directory']
 dataset = DeepONetDataset(config, device, dtype)
+N = dataset.N
 
 # Initialize the Network and the Optimizer (Adam)
 print('\nLoading the DeepONet...')
@@ -32,11 +33,18 @@ trunk_layers = [2, 512, 512, 512, 512, 2*p]
 network = DeepONet(branch_layers=branch_layers, trunk_layers=trunk_layers)
 network.load_state_dict(pt.load(store_directory + 'model_many_layers.pth', weights_only=True))
 
-# Evaluate network by generating a test forcing
+# Generate a proper forcing, on average 3 standard deviations away from the mean
 l = 0.12
-y_points = np.linspace(0.0, 1.0, dataset.N+1)
+y_points = np.linspace(0.0, 1.0, N+1)
 K = gp.precompute_covariance(y_points, l)
-forcing = gp.gp(y_points, K)
+R = np.real(sc.linalg.sqrtm(K))
+rng = rd.RandomState()
+z1 = 3.0 * rng.multivariate_normal(np.zeros(N+1), np.eye(N+1)) / np.sqrt(N+1)
+z2 = 3.0 * rng.multivariate_normal(np.zeros(N+1), np.eye(N+1)) / np.sqrt(N+1)
+forcing = (np.dot(R, z1), np.dot(R, z2))
+print('Number of Stdevs:', lg.norm(z1), lg.norm(z2))
+
+# Forward the forcing through the network
 branch_input = pt.tensor(np.concatenate(forcing), device=device, dtype=dtype)[None,:]
 nn_output_u, nn_output_v = network.forward(branch_input, dataset.trunk_input_data)
 nn_output_u = nn_output_u * dataset.scale_u
