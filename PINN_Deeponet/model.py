b8a05da01eb231cf701d1606e44e29f5734f6653
Hannes Vandecasteele <hannesvdc@gmail.com>
2025-06-24 12:25:10 -0400

    Implemented the physics loss using torch.func for jacobians and hessians

--- PINN_Deeponet/model.py
+++ PINN_Deeponet/model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 from torch.func import vmap, jacrev
 
 from collections import OrderedDict
+from typing import Tuple
 
 class BranchModel(nn.Module):
     """
@@ -70,7 +71,7 @@ class ConvDeepONet(nn.Module):
         return sum(p.numel() for p in self.parameters())
     
     # The input data: branch_input with shape (batch_size, 101), and trunk_input with shape (Nc, 2)
-    def forward(self, branch_input : pt.Tensor, trunk_input : pt.Tensor):
+    def forward(self, branch_input : pt.Tensor, trunk_input : pt.Tensor) -> Tuple[pt.Tensor, pt.Tensor]:
         branch_input = branch_input.unsqueeze(dim=1)
         branch_output = self.branch_net.forward(branch_input)
         trunk_output = self.trunk_net.forward(trunk_input)
@@ -92,7 +93,57 @@ class PhysicsLoss(nn.Module):
     def __init__(self, E, nu, w_int=1.0, w_dirichlet=1.0, w_forcing=1.0):
         super().__init__()
         self.E, self.nu = E, nu
+        self.pref = self.E / (1.0 - self.nu**2)
         self.w_int, self.w_dirichlet, self.w_forcing = w_int, w_dirichlet, w_forcing
 
     def forward(self, model : ConvDeepONet, f_batch : pt.Tensor, xy_int : pt.Tensor, xy_diriclet : pt.Tensor, xy_forcing : pt.Tensor):
-        pass
+
+        # PDE residual inside the domain
+        _, H_int = self.grads_and_hess(model, f_batch, xy_int)
+        u_xx = H_int[:,:,0,0,0]
+        u_xy = H_int[:,:,0,0,1]
+        u_yy = H_int[:,:,0,1,1]
+        v_xx = H_int[:,:,1,0,0]
+        v_xy = H_int[:,:,1,0,1]
+        v_yy = H_int[:,:,1,1,1]
+        loss_int_u = self.pref * (u_xx + 0.5*(1.0 + self.nu)*v_xy + 0.5*(1.0 - self.nu)*u_yy)
+        loss_int_v = self.pref * (v_yy + 0.5*(1.0 + self.nu)*u_xy + 0.5*(1.0 - self.nu)*v_xx)
+        loss_int = loss_int_u.square().mean() + loss_int_v.square().mean()
+        
+        # Dirichlet boundary conditions on the left
+        u_left, v_left = model.forward(f_batch, xy_diriclet)
+        loss_left = u_left.square().mean() + v_left.square().mean()
+
+        # Forcing (Neumann) boundary conditions on the right
+        J_forcing, _ = self.grads_and_hess(model, f_batch, xy_forcing)
+        u_x = J_forcing[:,:,0,0] # Shape (B, 101) because Nc = 101 on rhe right boundary
+        u_y = J_forcing[:,:,0,1]
+        v_x = J_forcing[:,:,1,0]
+        v_y = J_forcing[:,:,1,1]
+        loss_forcing_u = self.pref * (u_x + self.nu * v_y) + f_batch
+        loss_forcing_v = self.pref * (1.0 - self.nu) / 2.0 * (u_y + v_x)
+        loss_forcing = loss_forcing_u.square().mean() + loss_forcing_v.square().mean()
+
+        return self.w_int * loss_int + self.w_dirichlet * loss_left + self.w_forcing * loss_forcing
+
+    def grads_and_hess(self, model : ConvDeepONet, f_batch : pt.Tensor, xy : pt.Tensor) -> Tuple[pt.Tensor, pt.Tensor]:
+        """
+        model     : ConvDeepONet
+        f_batch   : (B, 101)
+        xy        : (Nc, 2)
+        returns   : J_v, H_v
+                        J_v  (B, Nc, 2, 2)   grad  [comp, dir ]
+                        H_v  (B, Nc, 2, 2, 2) Hess  [comp, d1 , d2]
+        """
+        def uv_vec(forcing : pt.Tensor, xy_single : pt.Tensor): # forcing has shape (101,) and xy_single (2,) -> need to unsqueeze
+            u, v = model.forward(forcing.unsqueeze(dim=0), xy_single.unsqueeze(dim=0)) # Both shape (1,1)
+            return pt.stack((u[0,0], v[0,0]))
+        
+        J_fn = jacrev(uv_vec, argnums=1) # Jacobian of (u, v) with shape (2, 2)
+        H_fn = jacrev(J_fn , argnums=1) # Hessian of (u, v) with shape (2, 2, 2)
+
+        # Map the Hessian to all inputs in forcing and xy_single
+        J = vmap(vmap(J_fn, (None,0)), (0,None))(f_batch, xy) # Shape (B, Nc, 2, 2)
+        H = vmap(vmap(H_fn, (None,0)), (0,None))(f_batch, xy) # Shape (B, Nc, 2, 2, 2)
+
+        return J, H
