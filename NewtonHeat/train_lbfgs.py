6222b921d37308b11c57107bd829b454a562d2c4
Hannes Vandecasteele <hannesvdc@gmail.com>
2026-01-24 16:27:42 -0500

    Improving readme.

--- NewtonHeat/train_lbfgs.py
+++ NewtonHeat/train_lbfgs.py
@@ -13,30 +13,34 @@ pt.set_default_dtype( dtype )
 pt.set_grad_enabled(True)
 
 store_directory = './Results/' 
-train_data = pt.load( store_directory + 'train_data.pth', weights_only=True )
-validation_data = pt.load( store_directory + 'validation_data.pth', weights_only=True )
+train_data = pt.load( store_directory + 'train_data.pth', weights_only=True, map_location=device )
+t_train = train_data[:,0:1]
+p_train = train_data[:,1:]
+validation_data = pt.load( store_directory + 'validation_data.pth', weights_only=True, map_location=device )
+t_validation = validation_data[:,0:1]
+p_validation = validation_data[:,1:]
 
 # Create the training and validation datasets
 B = 128
 N_validation = validation_data.shape[0]
-train_dataset = TensorDataset( train_data )
+train_dataset = TensorDataset( t_train, p_train )
 train_loader = DataLoader( train_dataset, batch_size=B, shuffle=False, drop_last=True )
-validation_dataset = TensorDataset( validation_data )
-validation_loader = DataLoader( validation_dataset, batch_size=N_validation, shuffle=False )
+validation_dataset = TensorDataset( t_validation, p_validation )
+validation_loader = DataLoader( validation_dataset, batch_size=N_validation, shuffle=False, drop_last=True )
 
 # Create the PINO model and loss
 n_hidden_layers = 2
 z = 64
 T_max = 10.0
 model = PINO( n_hidden_layers, z, T_max ).to( device=device )
-model.load_state_dict( pt.load( store_directory + 'model_adam.pth', weights_only=True) )
+model.load_state_dict( pt.load( store_directory + 'model_adam.pth', weights_only=True ) )
 loss_fn = PINOLoss()
 print('Number of Trainable Parameters: ', sum([ p.numel() for p in model.parameters() ])) 
 
 # Create the adam optimizer with learning rate scheduler
 lr = 1.0
 max_iter = 50
-n_epochs = 50
+n_epochs = 100
 optimizer = LBFGS( model.parameters(), lr, max_iter=max_iter )
 def getGradientNorm():
     grads = [p.grad.view(-1) for p in model.parameters() if p.grad is not None]
@@ -44,8 +48,6 @@ def getGradientNorm():
 
 # The L-BFGS closure
 def closure():
-    print('Model Evaluation...')
-
     model.train()
     optimizer.zero_grad()
 
@@ -59,7 +61,6 @@ def closure():
     
     # LBFGS only needs a scalar loss value back.
     # It does NOT need the returned tensor to carry a graph, since grads are already in .grad.
-    print('...Done')
     return pt.tensor(epoch_loss, device=device, dtype=dtype)
 def validate():
     print('Model Validation...')
@@ -75,13 +76,21 @@ def validate():
     return epoch_loss
 
 # Actual training and validation
+print('\nStarting Training...')
 train_counter = []
 train_losses = []
 validation_counter = []
 validation_losses = []
 for epoch in range( n_epochs ):
     training_loss = optimizer.step( closure )
+    print('Train Epoch: {} \tLoss: {:.4E}'.format( epoch, training_loss ))
+    
     validation_loss = validate()
+    print('Validation Epoch: {} \tLoss: {:.4E}'.format( epoch, validation_loss ))
+    
+    # Store the pretrained state
+    pt.save( model.state_dict(), store_directory + 'model_lbfgs.pth')
+    pt.save( optimizer.state_dict(), store_directory + 'optimizer_lbfgs.pth')
 
     train_counter.append( epoch )
     train_losses.append( training_loss )
--- README.md
+++ README.md
@@ -1,12 +1,16 @@
 # PINO - Physics-Informed Neural Operators
-This repository contains my experiments regarding Physics-Informed Neural Operators. Phyiscs-Informed learning has great potential, but for some reason I have never gotten it to work for a real differential equation or other physical model. The goal of this repository is to change that, starting small but with this express end goal of learning the 3D solution to a second-order PDE (TBD later)/
+This repository contains my experiments with Physics-Informed Neural Operators (PINOs). Physics-Informed learning has great potential, but for some reason I have never gotten it to work for a real physical model. My goal with this repository is to change that; starting small to master the basics but with the express end goal of learning the 3D solution to a second-order PDE. The precise PDE will be decided later, but it will be a real challenge! 
+
+Stay tuned and star this repo if you're interested in following my journey!
 
 There are currently two projects:
--  NewtonHeat contains a simple PINO that learns the solution T(t) to Newton's heat law dT/dt = -k * (T(t) - T_inf) with initial condition T(0) = T0. (T0, k, T_inf) are three model parameters and the neural network outputs T(t).
--  2D Steady-State Linear Elasticity equation. The code is spread over `PINN_DeepONet`, `Residual_PINN_Deeponet` and `FiLM_PINN`. This is mostly a failed project for now, which prompted me to go back to basics in the first example.
+-  `NewtonHeat` contains a simple PINO that learns the solution $T(t)$ to Newton's heat law $\frac{dT(t)}{dt} = -k * (T(t) - T_{\infty})$ with initial condition $T(0) = T0$. The PINO learns the temperature evolution for any combination of the three model parameteres $(T0, k, T_{\infty})$.
 
-More about these projects below. I will keep this repo and README updated as much as possible.
+-  2D Steady-State Linear Elasticity equation. The code is spread over `PINN_DeepONet`, `Residual_PINN_Deeponet` and `FiLM_PINN`. This is mostly a learning project for now. None of my approaches have worked yet, which prompted me to go back to basics in the first example.
 
-Note: An operator is a mathematical object that maps functions to functions. The term 'Physics-Informed Neural Operator' is therefore not always correct. For example, in Newton's Heat Law example, we map a discrete state of three parameters onto a scalar variable. The NN is a function, not an operator, but I will mostly ignore this abuse of notation.
+I explain these projects and my approach in much more detail below. I will keep the repo and README updated as much as possible.
+
+**Just me being pedantic** An operator is a mathematical object that maps functions to functions. The term 'Physics-Informed Neural Operator' is therefore not always correct. For example, in Newton's Heat Law example, we map a discrete state of three parameters onto a scalar variable. The NN is a function, not an operator, but I will mostly ignore this abuse of notation.
+
 
 ## Newton's Heat Law
