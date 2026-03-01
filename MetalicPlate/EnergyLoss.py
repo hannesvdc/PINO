Fixed a floating point issue in tensorized interpolation (specifically the GP kernel). Switching to training on CPU in double precision.

MetalicPlate/EnergyLoss.py
@@ -65,6 +65,8 @@ class EnergyLoss( nn.Module ):
         total_energy = x.new_zeros(())
         total_boundary = x.new_zeros(())
         total_weight = 0.0
+        traction_residual = 0.0
+        n_traction_terms = 0
 
         # Evaluate the model in the interior points and chunk over the branch inputs for memory reduction
         for s in range(0, Bb, self.branch_chunk):
@@ -72,8 +74,8 @@ class EnergyLoss( nn.Module ):
             w = float(e - s) / float(Bb)  # weight for mean reduction
 
             nu_c = nu[s:e,:]   # (Bc,1)
-            gx_c = gx[s:e,:]   # (Bc,Ny)
-            gy_c = gy[s:e,:]   # (Bc,Ny)
+            gx_c = gx[s:e,:]   # (Bc, n_grid_points)
+            gy_c = gy[s:e,:]   # (Bc, n_grid_points)
 
             # Evaluate model derivatives on interior points for this branch chunk
             _, _, u_x, u_y, v_x, v_y = grads_uv_jacrev(model, x, y, nu_c, gx_c, gy_c)  # (Bc,Bt)
@@ -90,8 +92,11 @@ class EnergyLoss( nn.Module ):
             strain_energy = 0.5 * ( C11 * (u_x**2 + v_y**2) + 2.0*C12*u_x*v_y + C66 * (u_y + v_x)**2 )
             strain_integral = pt.sum(strain_energy * mc_weights[None,:], dim=1) / pt.sum( mc_weights )
 
-            gx_int, gy_int = tensorizedRBFInterpolator( self.cholesky_L, self.y_grid, self.l, bc_y, gx_c, gy_c )                                                    # (Bc,Bt_bc) each
+            gx_int, gy_int = tensorizedRBFInterpolator( self.cholesky_L, self.y_grid, self.l, bc_y, gx_c, gy_c )    
             bc_integral = (gx_int * u_bc + gy_int * v_bc).mean(dim=1)  # (Bc,)
+
+            assert gx_int.shape == u_bc.shape, f"gx_int {gx_int.shape} vs u_bc {u_bc.shape}"
+            assert gy_int.shape == v_bc.shape, f"gy_int {gy_int.shape} vs v_bc {v_bc.shape}"
 
             # Accumulate means (keep grads)
             total_energy = total_energy + w * strain_integral.mean()
@@ -102,16 +107,18 @@ class EnergyLoss( nn.Module ):
             nu_b = nu_c[:, 0][:, None]  # (Bc,1) broadcast over Bt_bc
             t_x = (1.0 / (1.0 - nu_b**2)) * (u_xb + nu_b * v_yb)                 # (Bc,Bt_bc)
             t_y = (1.0 / (2.0 * (1.0 + nu_b))) * (u_yb + v_xb)                   # (Bc,Bt_bc)
-            traction_residual = ((t_x - gx_int)**2 + (t_y - gy_int)**2).mean()  / ( gx_int**2 + gy_int**2 ).mean() # (Bc,)
+            traction_residual += ((t_x - gx_int)**2 + (t_y - gy_int)**2).mean()  / ( gx_int**2 + gy_int**2 ).mean() # float
+            n_traction_terms += nu_b.shape[0]
 
             # free refs early for memory
             del u_x, u_y, v_x, v_y, u_bc, v_bc, gx_int, gy_int, strain_energy
 
         avg_energy = total_energy / total_weight
         avg_boundary = total_boundary / total_weight
+        traction_residual /= n_traction_terms
         loss = avg_energy - avg_boundary
         loss_info = {"energy" : float(avg_energy.detach().item()), "boundary" : float(avg_boundary.detach().item()), 
-                     "rms" : float(loss.detach().item()), "traction" : float(traction_residual.detach().item())}
+                     "rms" : float(loss.detach().item()), "traction" : float(traction_residual)}
         return loss, loss_info
 
 def grads_uv_jacrev(model, x, y, nu, gx, gy):